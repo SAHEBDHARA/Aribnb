b9c07b36fd3a450e365de90df1a010570be4b507
Saheb Dhara <87798754+SAHEBDHARA@users.noreply.github.com>
2023-11-18 00:29:08 +0530

    added register funtionality

--- pland/app/components/Models/RegisterModel.tsx
+++ pland/app/components/Models/RegisterModel.tsx
@@ -90,6 +90,12 @@ export const RegisterModel = () => {
             icon={AiFillGithub}
             onClick={()=>{}}
             />
+            <div className="text-neutral-500 text-center mt-4 font-light">
+                <div className="flex flex-row items-center gap-2 justify-center">
+                    <div>Already Registered?</div>
+                    <div className="text-neutral-800 cursor-pointer hover:underline" onClick={registerModel.onClose}>Log in</div>
+                </div>
+            </div>
         </div>
     )
 
---
+++ pland/pages/api/auth/[...nextauth].ts
@@ -0,0 +1,59 @@
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { AuthOptions } from "next-auth";
+import  prisma  from "@/app/libs/prismadb";
+import GithubProvider from 'next-auth/providers/github'
+import GoogleProvider from 'next-auth/providers/google'
+import CredentialsProvider from 'next-auth/providers/credentials'
+import bcript from 'bcrypt'
+import NextAuth from "next-auth";
+
+
+export const authOptions: AuthOptions = {
+ adapter: PrismaAdapter(prisma),
+ providers: [
+    GithubProvider({
+        clientId: process.env.GITHUB_ID as string,
+        clientSecret: process.env.GITHUB_SECRET as string,
+    }),
+    GoogleProvider({
+        clientId: process.env.GOOGLE_CLIENT_ID as string,
+        clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    }),
+    CredentialsProvider({
+        name: 'credentials',
+        credentials:{
+            email: { label: 'email', type: 'text'},
+            password: { label: 'password', type: 'password'},
+        },
+        async authorize(credentials){
+            if(!credentials?.email || !credentials?.password){
+                throw new Error('Invalid credentials')
+            }
+            const user = await prisma.user.findUnique({
+                where:  {email: credentials.email}
+            })
+            if(!user || !user?.hashedPassword){
+                throw new Error('Invalid credentials')
+            }
+            const isCorrectPassword = await bcript.compare(
+                credentials.password,
+                user.hashedPassword,
+            )
+            if(!isCorrectPassword){
+                throw new Error('Invalid credentials');
+            }
+            return user
+        }
+    })
+ ],
+ pages: {
+    signIn: '/'
+ },
+ debug: process.env.NODE_ENV == 'production',
+ session: {
+    strategy: "jwt"
+ },
+ secret: process.env.NEXTAUTH_SECRET
+}
+
+export default NextAuth(authOptions)
